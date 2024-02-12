aa0542c5aa1000df4a25b4d56faca27f88a2925a
Louis Mony <73167166+LouisMony@users.noreply.github.com>
2024-02-12 15:31:31 +0100

    front register pages

--- src/pages/Register/SignIn.jsx
+++ src/pages/Register/SignIn.jsx
@@ -1,11 +1,61 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import '../../style/StyleRegister.scss';
 
 const SignIn = () => {
-  return (
-    <div>
-      Je suis la page signIn
-    </div>
-  )
-}
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
 
-export default SignIn
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('FormData:', formData);
+  };
+
+  return (
+    <div className='register'>
+      <h2>Me connecter</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <span>Email</span>
+          <input
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
+        </label>
+
+        <label>
+          <span>Mot de passe</span>
+          <input
+            type="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+            required
+          />
+        </label>
+
+        <button type="submit">Me connecter</button>
+      </form>
+      {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
+      <Link to={'/register/creer-un-compte'}>Je n'ai pas encore de compte.</Link>
+    </div>
+  );
+};
+
+export default SignIn;
+
--- src/pages/Register/SignUp.jsx
+++ src/pages/Register/SignUp.jsx
@@ -1,11 +1,89 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import '../../style/StyleRegister.scss';
 
 const SignUp = () => {
-  return (
-    <div>
-      je suis le signup
-    </div>
-  )
-}
+  const [formData, setFormData] = useState({
+    username: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+  });
 
-export default SignUp
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (formData.password !== formData.confirmPassword) {
+      setErrorMessage("Les mots de passe ne correspondent pas.");
+    } else {
+      console.log('FormData:', formData);
+      setErrorMessage('');
+    }
+  };
+
+  return (
+    <div className='register'>
+      <h2>Créer un compte</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <span>Nom d'utilisateur</span>
+          <input
+            type="text"
+            name="username"
+            value={formData.username}
+            onChange={handleChange}
+            required
+          />
+        </label>
+
+        <label>
+          <span>Email</span>
+          <input
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
+        </label>
+
+        <label>
+          <span>Mot de passe</span>
+          <input
+            type="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+            required
+          />
+        </label>
+
+        <label>
+          <span>Confirmation de mot de passe:</span>
+          <input
+            type="password"
+            name="confirmPassword"
+            value={formData.confirmPassword}
+            onChange={handleChange}
+            required
+          />
+        </label>
+        <button type="submit">S'inscrire</button>
+      </form>
+      {errorMessage && <p className='formError_p'>{errorMessage}</p>}
+      
+      <Link to={'/register/me-connecter'}>J'ai déja un compte.</Link>
+    </div>
+  );
+};
+
+export default SignUp;
