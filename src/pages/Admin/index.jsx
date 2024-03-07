1d358b64d8319d160e243baf6f81b1df93139ed3
Louis Mony <73167166+LouisMony@users.noreply.github.com>
2024-03-07 08:27:28 +0100

    add userSubscription Table

--- src/pages/Admin/index.jsx
+++ src/pages/Admin/index.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../../style/StyleAdmin.scss';
 import { useSelector } from 'react-redux';
 import { formatterDate } from '../../js/helpers';
 
 const Admin = () => {
   const eventsSelector = useSelector(state => state.events.data);
+  const inscriptionSelector = useSelector(state => state.inscriptions.data)
+
+  const [eventInscriptions, setEventInscriptions] = useState(null)
+
+  const handleSelectEvent = (id) =>{
+    console.log(id);
+    setEventInscriptions(inscriptionSelector.filter(inscription => inscription.idEvent === id));
+  }
 
   return (
     <div className='admin'>
@@ -20,19 +28,41 @@ const Admin = () => {
           </thead>
           <tbody>
             {eventsSelector ? eventsSelector.map((item, index) => (
-                <tr>
+                <tr key={item.id} onClick={() => handleSelectEvent(item.id)}>
                     <td>{item.name}</td>
                     <td>{formatterDate(item.date)}</td>
                     <td>{item.reservations}/{item.places}</td>
                     <td>{item.reservations}</td>
-                    <td><button className='mainButton'>Modifier</button></td>
+                    <td><button>Modifier</button></td>
+                </tr>
+              )) :
+                <span className="loader"></span>
+            }        
+          </tbody>
+      </table>
+      
+      {inscriptionSelector && eventInscriptions && eventInscriptions.length > 0 && <table>
+          <thead>
+              <tr>
+                  <th>Username</th>
+                  <th>Email</th>
+                  <th>État</th>
+              </tr>
+          </thead>
+          <tbody>
+            {inscriptionSelector && eventInscriptions ? eventInscriptions.map((item, index) => (
+                <tr key={item.id}>
+                    <td>{item.id}</td>
+                    <td>{item.mailUser}</td>
+                    <td>{item.isOnWaitingList ? "Fil d'attente" : "Inscrit"}</td>
                 </tr>
               )) :
                 <span className="loader"></span>
             }
               
           </tbody>
-      </table>
+      </table>}
+
     </div>
   )
 }
